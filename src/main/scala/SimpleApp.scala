3c5d714457da7dd1bf4c466e81e362e5efcaf16d
flyingbucket <menglinli598@gmail.com>
2025-12-23 11:11:26 +0800

    fix: main method return type warning

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -2,7 +2,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 object SimpleApp {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     val logFile = "hdfs://master:9000/input/LICENSE.txt" // 用于统计的文本文件
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
