ae020875044ab03e360c5a1634c6fdf682c675fa
flyingbucket <menglinli598@gmail.com>
2026-01-14 13:14:58 +0800

    format code

--- scripts/MCMC.scala
+++ scripts/MCMC.scala
@@ -3,18 +3,20 @@ import breeze.numerics._
 import breeze.stats.distributions._
 import java.util.concurrent.ThreadLocalRandom
 
-
 def targetDensity(x: Double): Double = {
-  if (x >= 0 && x<=2){
-	math.pow(2 - x, 2) * math.pow(x * x * x + 1, 3)
-	} else
-   {
-	1e-8
-   }
+  if (x >= 0 && x <= 2) {
+    math.pow(2 - x, 2) * math.pow(x * x * x + 1, 3)
+  } else {
+    1e-8
+  }
 }
 
-
-def metropolisHastings(targetDensity: Double => Double, initial: Double, iterations: Int, proposalStd: Double): DenseVector[Double] = {
+def metropolisHastings(
+    targetDensity: Double => Double,
+    initial: Double,
+    iterations: Int,
+    proposalStd: Double
+): DenseVector[Double] = {
   val samples = DenseVector.zeros[Double](iterations)
   var current = initial
   val rand = ThreadLocalRandom.current
@@ -33,12 +35,11 @@ def metropolisHastings(targetDensity: Double => Double, initial: Double, iterati
   samples
 }
 
-
 val initialSample = 0.0
 val iterations = 10000
 val proposalStd = 0.5
 
-val samples = metropolisHastings(targetDensity, initialSample, iterations, proposalStd)
+val samples =
+  metropolisHastings(targetDensity, initialSample, iterations, proposalStd)
 
 println(samples(0 to 10))
-
--- scripts/slice_sampler.scala
+++ scripts/slice_sampler.scala
@@ -1,43 +1,42 @@
 import java.util.concurrent.ThreadLocalRandom
 
 def targetDensity(x: Double): Double = {
-  if (x >= 0 && x<=2){
-	math.pow(2 - x, 2) * math.pow(x * x * x + 1, 3)
-	} else
-   {
-	1e-8
-   }
+  if (x >= 0 && x <= 2) {
+    math.pow(2 - x, 2) * math.pow(x * x * x + 1, 3)
+  } else {
+    1e-8
+  }
 }
-  // 定义采样函数
+// 定义采样函数
 def sliceSampling(iterations: Int, width: Double): List[Double] = {
-   var samples: List[Double] = List.empty
-   var current = 1.0  // 初始样本
-   val rand = ThreadLocalRandom.current
+  var samples: List[Double] = List.empty
+  var current = 1.0 // 初始样本
+  val rand = ThreadLocalRandom.current
 
-   for (_ <- 0 until iterations) {
-      val height = rand.nextDouble*targetDensity(current)
-      var interval = (current - width * rand.nextDouble(), current + width * rand.nextDouble())
+  for (_ <- 0 until iterations) {
+    val height = rand.nextDouble * targetDensity(current)
+    var interval =
+      (current - width * rand.nextDouble(), current + width * rand.nextDouble())
 
-      var newX = interval._1 + (interval._2 - interval._1) * rand.nextDouble()
-      var newY = targetDensity(newX)
+    var newX = interval._1 + (interval._2 - interval._1) * rand.nextDouble()
+    var newY = targetDensity(newX)
 
-      while (newY < height) {
-        if (newX > current) interval = (interval._1, newX)
-        else interval = (newX, interval._2)
+    while (newY < height) {
+      if (newX > current) interval = (interval._1, newX)
+      else interval = (newX, interval._2)
 
-        newX = interval._1 + (interval._2 - interval._1) * rand.nextDouble()
-        newY = targetDensity(newX)
-      }
+      newX = interval._1 + (interval._2 - interval._1) * rand.nextDouble()
+      newY = targetDensity(newX)
+    }
 
-      current = newX
-      samples = current :: samples
-   }
-   samples.reverse
+    current = newX
+    samples = current :: samples
+  }
+  samples.reverse
 }
 
-
-val iterations = 10000   // 迭代次数
-val width = 0.1          // 切片的宽度
+val iterations = 10000 // 迭代次数
+val width = 0.1 // 切片的宽度
 
 val samples = sliceSampling(iterations, width)
 
